c15e0592fd88de75840cf1a7787391b6b96c1744
Matheus Cunha <mathcunha@gmail.com>
2015-01-14 00:55:39 +0000

    getting port from system env

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
 	"github.com/mathcunha/bandeiraenergia/band"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", band.DoRequest)
 	probe := band.Probe{}
 	probe.Run()
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(GetPort(), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: 0.0.0.0:8080 ", err)
 	}
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8080"
+		log.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
