39e0e72bf2d0b2ebc0d5490bfa11f5bd1a730644
Matheus Cunha <mathcunha@gmail.com>
2015-01-14 00:31:46 -0200

    Update probe.go

--- band/probe.go
+++ band/probe.go
@@ -3,7 +3,7 @@ package band
 type Probe struct{}
 
 func (p Probe) Run() {
-	currentStatus = []Status{Status{0, 0}, Status{1, 0}, Status{2, 0}, Status{3, 0}, Status{4, 1}}
+	currentStatus = []Status{Status{0, 0}, Status{1, 0}, Status{2, 0}, Status{3, 0}, Status{4, 0}}
 }
 
 func (p Probe) Interval() string {
